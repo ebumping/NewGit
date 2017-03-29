0b60cd05c10786f245392bfa7bb3a85880f36031
sdylanjeffery <sdylanjeffery@C218-1762.yccc.edu>
2017-03-29 19:42:05 -0400

    Work

--- NewGit/src/books/Book.java
+++ NewGit/src/books/Book.java
@@ -1,5 +1,70 @@
 package books;
 
-public class Book {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+//Allows you to call book class and input data into your list that corresponds to the title of choice
+
+public class Book {
+	private String title;
+	private String author;
+	private long isbn;
+	private String publisher;
+	private Date pubDate;
+	
+	DateFormat df = new SimpleDateFormat("MM/DD/YYYY");
+	
+	public Book(String title, String author, long isbn, String publisher, Date pubDate){
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.publisher = publisher;
+		this.pubDate = pubDate;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public long getIsbn() {
+		return isbn;
+	}
+	
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+	
+	public String getPublisher() {
+		return publisher;
+	}
+	
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	public Date getPubDate() {
+		return pubDate;
+	}
+	
+	public void setPubDate(Date pubDate) {
+		this.pubDate = pubDate;
+	}
+	
+	public String toString(){
+		return "Title: " + title + "\nAuthor: " + author + "\nISBN : " + isbn + "\nPublisher : " + publisher + "\nPublishing Date : " + pubDate + "\n";
+	}
+	
 }
--- NewGit/src/books/BookAssignment.java
+++ NewGit/src/books/BookAssignment.java
@@ -1,5 +1,51 @@
 package books;
 
-public class BookAssignment {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
+public class BookAssignment {
+	DateFormat df = new SimpleDateFormat("MM/DD/YYYY");
+	private List<Book> books = new ArrayList<Book>();
+	
+	private static Comparator<Book> SORT_BY_AUTHOR = new Comparator<Book>(){
+
+		@Override
+		public int compare(Book o1, Book o2) {
+			return o1.getAuthor().compareTo(o2.getAuthor());
+		}
+		
+	};
+	
+	public void list(){
+		for(Book x : books){
+			System.out.println(x);
+		}
+	}
+	
+	private void loadBooks(){
+		try {
+			//this one's for you Kyle
+			books.add(new Book("50 Shades of Grey","E L James", 9780345803481L, "Knopf Doubleday Publishing Group", df.parse("04/03/2012")));
+																// 'L' is required at the end of long integer to allow the compiler to recognize it as a long and not an int
+			books.add(new Book("1984", "George Orwell", 9780451524935L , "Signet Classic", df.parse("01/01/1961")));
+			books.add(new Book("Atlas Shrugged", "Ayn Rand", 9780451191144L, "Signet", df.parse("10/10/1957")));
+			books.add(new Book("The Fountainhead", "Ayn Rand", 9780451191151L, "Signet", df.parse("06/07/1943")));
+			books.add(new Book("Anthem", "Ayn Rand", 9781503250703L, "Cassell", df.parse("01/01/1938")));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] argz){
+		BookAssignment ba = new BookAssignment();
+		ba.loadBooks();
+		ba.list();
+	}
+	
 }
